573442fd7052eaff13985803ea7ed82514e477a5
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-03-23 18:50:52 +0530

    Add files via upload

---
+++ 99.py
@@ -0,0 +1,8 @@
+#Factorial of each number from 1 to n
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    fact = 1
+    for j in range(1, i + 1):
+        fact *= j
+    print(f"Factorial of {i} = {fact}")
