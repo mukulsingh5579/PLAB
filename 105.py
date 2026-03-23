da952b5d2591fd9902d3e463de85eb903d99a244
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-03-23 19:02:20 +0530

    Add files via upload

---
+++ 105.py
@@ -0,0 +1,8 @@
+#Sum of digits of a number using recursion
+def sum_digits(n):
+    if n == 0:
+        return 0
+    return (n % 10) + sum_digits(n // 10)
+
+num = int(input("Enter a number: "))
+print("Sum of digits =", sum_digits(num))
