3fbbb62feda21e5033406a2eba57e8939c3fda79
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-03-29 22:07:47 +0530

    Add files via upload

---
+++ 137.py
@@ -0,0 +1,14 @@
+#Count even and odd elements
+n = int(input("Enter number of elements: "))
+arr = list(map(int, input("Enter elements: ").split()))
+
+even = odd = 0
+
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even)
+print("Odd:", odd)
