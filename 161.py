d4c85c74bad07c28487ff051fff90f4ae7afda76
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-03 19:25:44 +0530

    Add files via upload

---
+++ 161.py
@@ -0,0 +1,8 @@
+#Check if two arrays are equal (same elements & order)
+arr1 = [1, 2, 3, 4]
+arr2 = [1, 2, 3, 4]
+
+if arr1 == arr2:
+    print("Arrays are equal")
+else:
+    print("Arrays are not equal")
