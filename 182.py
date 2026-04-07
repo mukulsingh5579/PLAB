e2b88711631da9755e1b3652d869ff25e11ea176
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-07 22:48:01 +0530

    Add files via upload

---
+++ 182.py
@@ -0,0 +1,7 @@
+#Find Largest of 3 Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+print("Largest number is:", largest)
