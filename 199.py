e2b6e7620dae65cdbee05eb41287af649672febb
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-10 22:28:54 +0530

    Add files via upload

---
+++ 199.py
@@ -0,0 +1,13 @@
+#Pascal’s Triangle
+n = 5
+
+for i in range(n):
+    num = 1
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(i + 1):
+        print(num, end=" ")
+        num = num * (i - j) // (j + 1)
+
+    print()
