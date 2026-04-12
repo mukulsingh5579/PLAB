3e065f14c348a131af6b74de1c28790d3c05f379
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-12 21:49:43 +0530

    Add files via upload

---
+++ 208.py
@@ -0,0 +1,10 @@
+#Print All Subarrays
+arr = [1, 2, 3]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(i, n):
+        for k in range(i, j + 1):
+            print(arr[k], end=" ")
+        print()
