66302ccb096d2101b1479682c8f9e13d2965fe76
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-16 22:09:01 +0530

    Add files via upload

---
+++ 229.py
@@ -0,0 +1,13 @@
+#Count Frequency of Each Character
+# Count character frequency
+s = input("Enter a string: ")
+
+freq = {}
+
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+print(freq)
