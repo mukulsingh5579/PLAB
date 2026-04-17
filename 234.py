0abc13ff176d4f6ce9a04efe2b34c2393e53588a
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-17 22:32:21 +0530

    Add files via upload

---
+++ 234.py
@@ -0,0 +1,8 @@
+#Check if String is Anagram
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+if sorted(s1) == sorted(s2):
+    print("Anagram")
+else:
+    print("Not Anagram")
