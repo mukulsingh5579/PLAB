6eb4a459245d2132c0d6dd6bfd24fdd9a0587e2d
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-21 23:05:09 +0530

    Add files via upload

---
+++ 248.py
@@ -0,0 +1,23 @@
+#Simple Calculator (Using Functions)
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero not allowed"
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", add(a, b))
+print("Subtraction:", sub(a, b))
+print("Multiplication:", mul(a, b))
+print("Division:", div(a, b))
