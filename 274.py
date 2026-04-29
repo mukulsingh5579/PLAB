a6c4273c376006f699a9274f3f465256780fab3d
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-04-29 21:46:11 +0530

    Add files via upload

---
+++ 274.py
@@ -0,0 +1,7 @@
+#f-Strings with Debugging Support
+user_name = "Alex"
+login_attempts = 3
+
+# Adding '=' prints the variable name and the value automatically
+print(f"{user_name=}, {login_attempts=}")
+# Output: user_name='Alex', login_attempts=3
