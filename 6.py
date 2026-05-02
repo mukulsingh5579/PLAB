d1644ef78048bd69e9fd98e0f142a14555fe602c
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-05-02 21:54:07 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,10 @@
+#Dictionary & Data Storage
+user = {
+    "name": "Alex",
+    "age": 24,
+    "role": "Developer",
+    "skills": ["Python", "JavaScript", "SQL"]
+}
+
+print(f"User Name: {user['name']}")
+print(f"Primary Skill: {user['skills'][0]}")
