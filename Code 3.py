4c591798f64b0ad7aeee49d1c0c417cad0a74656
mukulsingh5579 <mukulsingh5579@gmail.com>
2026-05-30 23:41:04 +0530

    Add files via upload

---
+++ Code 3.py
@@ -0,0 +1,9 @@
+# Ask the user for two numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert the inputs from strings to decimals (floats) and add them
+sum_result = float(num1) + float(num2)
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum_result}")
